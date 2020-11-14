e9684b10c34e12b6aeabf660848ebc8cbe63756a
Jan H <janh@example.com>
2020-11-14 13:45:17 +0100

    navi ts quick fix

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./navigation.component.scss'],
 })
 export class NavigationComponent implements OnInit {
-    public navPoints: string[];
+    public navPoints: string[] = [];
 
     constructor() {}
 
